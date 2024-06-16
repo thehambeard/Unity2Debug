393433a2f8494405a73e2178fe33d10b045d261f
thehambeard <61787444+thehambeard@users.noreply.github.com>
2024-06-16 06:52:29 -0400

    Update OwlCatUtil.cs

--- Unity2Debug.Common/Utility/OwlCatUtil.cs
+++ Unity2Debug.Common/Utility/OwlCatUtil.cs
@@ -11,25 +11,17 @@ namespace Unity2Debug.Common.Utility
 {
     internal class OwlCatUtil
     {
-        public static string FindWrathPath()
-        {
-            var path = FindInstallLocation("Pathfinder Wrath Of The Righteous", "Player.log", "Mono path[0]", @"^Mono path\[0\] = '(.*?)/Wrath_Data/Managed'$");
+        public static string FindWrathPath() => FindInstallLocation("Pathfinder Wrath Of The Righteous", "Player.log", @"^Mono path\[0\] = '(.*?)/Wrath_Data/Managed'$");
+        public static string FindRTPath() => FindInstallLocation("Warhammer 40000 Rogue Trader", "Player.log", @"^Mono path\[0\] = '(.*?)/WH40KRT_Data/Managed'$");
+        public static string FindKMPath() => FindInstallLocation("Pathfinder Kingmaker", "output_log.txt", @"^Mono path\[0\] = '(.*?)/Kingmaker_Data/Managed'$");
 
-            if(string.IsNullOrEmpty(path))
-                path = FindInstallLocation("Pathfinder Wrath Of The Righteous", "Player.log", "Mono path[0]", @"^Mono path\[0\] = '(.*?)/wrath_Data/Managed'$");
-
-            return path;
-        }
-        public static string FindRTPath() => FindInstallLocation("Warhammer 40000 Rogue Trader", "Player.log", "Mono path[0]", @"^Mono path\[0\] = '(.*?)/WH40KRT_Data/Managed'$");
-        public static string FindKMPath() => FindInstallLocation("Pathfinder Kingmaker", "output_log.txt", "Mono path[0]", @"^Mono path\[0\] = '(.*?)/Kingmaker_Data/Managed'$");
-
-
-        private static string FindInstallLocation(string gameFolderName, string logName, string line, string regex)
+        private static string FindInstallLocation(string gameFolderName, string logName, string regex)
         {
             if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
             {
                 var rogueTraderDataPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "Low", "Owlcat Games", gameFolderName, logName);
                 string lineToFind = "Mono path[0]";
+                var line = string.Empty;
                 foreach (var lineIter in File.ReadLines(rogueTraderDataPath))
                 {
                     if (lineIter.Contains(lineToFind))
@@ -38,8 +30,7 @@ namespace Unity2Debug.Common.Utility
                         break;
                     }
                 }
-                string monoPathRegex = regex;
-                Match match = Regex.Match(line, monoPathRegex);
+                Match match = Regex.Match(line, regex);
                 if (match.Success)
                 {
                     return match.Groups[1].Value;
